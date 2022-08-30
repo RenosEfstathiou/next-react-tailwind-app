2e5b17f382215eb4ab36a770738dfa09365d5db6
unknown <it164826@it.teithe.gr>
2022-08-30 09:34:57 +0300

    Made small changes in the header component

--- next-project/components/Header.tsx
+++ next-project/components/Header.tsx
@@ -50,27 +50,26 @@ const Header = () => {
       <Head>
         <title>Recipe</title>
         <meta name="description" content="A recipe finding app" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/recipe.png" />
       </Head>
 
       <Popover className="sticky z-10 bg-white top-0 left-0 border-b">
-        <div className="flex items-center justify-between px-4 py-6 sm:px-6 md:justify-start md:space-x-10">
+        <div className="flex items-center justify-between px-4 py-3 sm:px-6  md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="#">
-              <span className="sr-only">Workflow</span>
-              <img
-                className="h-8 w-auto sm:h-10"
-                src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
-                alt=""
-              />
-            </a>
+            <Link
+              href="/"
+            >
+              <Image src='/recipe.png' width={50} height={50} className="cursor-pointer"></Image>
+            </Link>
           </div>
+
           <div className="-my-2 -mr-2 md:hidden">
             <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
               <span className="sr-only">Open menu</span>
               <Bars3Icon className="h-6 w-6" aria-hidden="true" />
             </Popover.Button>
           </div>
+
           <Popover.Group as="nav" className="hidden space-x-10 md:flex">
             <Popover className="relative">
               {({ open }) => (
@@ -100,7 +99,7 @@ const Header = () => {
                     leaveFrom="opacity-100 translate-y-0"
                     leaveTo="opacity-0 translate-y-1"
                   >
-                    <Popover.Panel className="absolute z-10 -ml-4 mt-8 w-screen max-w-md transform lg:left-1/2 lg:ml-0 lg:max-w-xl lg:-translate-x-[32.5rem]">
+                    <Popover.Panel className="absolute z-10 -ml-4 mt-8 w-screen max-w-md transform lg:left-1/2 lg:ml-0 lg:max-w-xl lg:-translate-x-[32.5rem] md:left-1/2 md:ml-0 md:max-w-xl md:-translate-x-[32.5rem]">
                       <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-y-scroll h-[30rem]">
                         <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8 lg:grid-cols-2">
                           {categories.map((category) => (
@@ -110,7 +109,7 @@ const Header = () => {
                             >
                               <div className="-m-3 flex items-center rounded-lg p-3 hover:bg-gray-50 cursor-pointer">
                                 <div className="h-[80px] w-[80px] items-center justify-center rounded-md text-white shadow">
-                                  <Image src={category.strCategoryThumb} width='100' height='100' layout="responsive" objectFit="cover" alt={`${category.strCategory}_thumb`} />
+                                  <Image src={category.strCategoryThumb} width={100} height={100} layout="responsive" objectFit="cover" alt={`${category.strCategory}_thumb`} />
                                 </div>
                                 <div className="ml-4">
                                   <p className="text-base text-2xl font-bold text-gray-900">{category.strCategory}</p>
@@ -141,13 +140,11 @@ const Header = () => {
             <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
               <div className="px-5 pt-5 pb-6">
                 <div className="flex items-center justify-between">
-                  <div>
-                    <img
-                      className="h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
-                      alt="Workflow"
-                    />
-                  </div>
+                  <Link
+                    href="/"
+                  >
+                    <Image src='/recipe.png' width={50} height={50} className="cursor-pointer"></Image>
+                  </Link>
                   <div className="-mr-2">
                     <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                       <span className="sr-only">Close menu</span>
@@ -156,7 +153,7 @@ const Header = () => {
                   </div>
                 </div>
                 <div className="mt-6">
-                  <nav className="grid grid-cols-1 gap-7">a
+                  <nav className="grid grid-cols-1 gap-7 overflow-hidden overflow-y-scroll h-[86vh]">
                     {categories.map((category) => (
                       <Link
                         key={category.idCategory}
@@ -164,8 +161,8 @@ const Header = () => {
 
                       >
                         <div className="-m-3 flex items-center rounded-lg p-3 hover:bg-gray-50">
-                          <div className="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-md bg-indigo-500 text-white">
-
+                          <div className="h-24 w-24 flex-shrink-0 items-center justify-center rounded-md text-white">
+                            <Image src={category.strCategoryThumb} width={24} height={24} layout="responsive" objectFit="cover" alt={`${category.strCategory}_thumb`} />
                           </div>
                           <div className="ml-4 text-base font-medium text-gray-900">{category.strCategory}</div>
                         </div>
